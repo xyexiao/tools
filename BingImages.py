879ae21fe4ba0236843aa7393928c0b1de52b2fb
XYe <1036574443@qq.com>
2020-05-01 20:06:02 +0800

    init

---
+++ BingImages.py
@@ -0,0 +1,63 @@
+# `coding:utf-8
+'''
+https://www.bing.com/HPImageArchive.aspx?
+idx=0&          #起始序号
+n=8&            #图片总数，最多为8
+format=js&      #返回数据格式
+pid=HpEdgeAn&   
+mkt=zh-cn
+'''
+import requests
+import os
+import shutil
+
+
+baseURL = "https://www.bing.com"
+API = "https://www.bing.com/HPImageArchive.aspx?idx=%d&n=%d&format=js"
+# 这个API通过调整参数可以总共获取15张图片信息
+
+
+def getImageInfo(respons_json):
+    # 获取响应的json数据中图片的信息
+    images = []
+    for info in respons_json["images"]:
+        images.append({
+            "title": info["copyright"].split("©")[0][:-2] + ".jpg",
+            "url": baseURL + info["url"]
+        })
+    return images
+
+
+def downloadImage(url, save_dir, title):
+    # 下载图片
+    respons = requests.get(url)
+    with open(os.path.join(save_dir, title), "wb") as f:
+        for chunk in respons.iter_content(1024):
+            f.write(chunk)
+
+
+def main(images_number=15, save_dir="", cover=False):
+    respons = requests.get(API % (0, 7))
+    images = getImageInfo(respons.json())
+    respons = requests.get(API % (7, 8))
+    images += getImageInfo(respons.json())
+    if save_dir:
+        if not os.path.isabs(save_dir):
+            raise Exception("保存路径应为完整路径")
+        if not os.path.isdir(save_dir):
+            raise Exception("保存路径不存在")
+        if cover:
+            shutil.rmtree(save_dir)
+            os.mkdir(save_dir)
+    else:
+        save_dir = os.path.join(os.getcwd(), "BingImages")
+        if not os.path.isdir(save_dir):
+            os.mkdir(save_dir)
+        if cover:
+            shutil.rmtree(save_dir)
+            os.mkdir(save_dir)
+    for image in images[:images_number]:
+        downloadImage(image["url"], save_dir, image["title"])
+
+if __name__ == '__main__':
+    main(cover=True)
